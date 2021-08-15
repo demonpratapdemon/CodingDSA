43092960fbe534ac166245235d0aa8957fdf9c19
demonpratapdemon <pratapkumarchandra.96@gmail.com>
2021-08-15 13:22:41 +0530

    Commit : GFG problem of the day - 13, Aug, 2021

---
+++ src/problem_of_the_day/NumberOfPalindromicStrings.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package problem_of_the_day;
+
+import java.util.HashMap;
+import java.util.TreeSet;
+
+/**
+ * @author PRATAP
+ *
+ */
+public class NumberOfPalindromicStrings {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int N = 19, K = 14;
+		int ans = palindromicStrings(N, K);
+		System.out.println(ans);
+	}
+
+	public static int palindromicStrings(int N, int K) {
+		// code here
+		long mod = (long) (1e9 + 7);
+		long[] dp = new long[N + 1];
+		if (N == 1)
+			return K;
+		dp[1] = K % mod;
+		dp[2] = K % mod;
+		long sum = (dp[1] + dp[2]) % mod;
+		for (int i = 3; i <= N; i++) {
+			if (i % 2 == 0) {
+				dp[i] = dp[i - 1];
+			} else {
+				int temp = (i / 2);
+				dp[i] = (dp[i - 1] * ((K - temp) % mod)) % mod;
+			}
+			sum = (sum + dp[i]) % mod;
+		}
+		return (int)(sum % mod);
+	}
+
+}
